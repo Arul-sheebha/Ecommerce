a42ac4a04519c62b5cfb60b87b6540191c7625d2
A.Arul Sheebha <156940737+Arul-sheebha@users.noreply.github.com>
2025-01-23 13:09:21 +0530

    portfolio

---
+++ portfolio/contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="styles.css">
+		<title>your portfolio</title>
+	</head>
+	<body>
+		
+					<h2>Contact</h2>
+					Email  : <p>arulsheebhaa@gmail.com</p>
+					GitHub : <p>github.com / sheebha</p>
+					Phone  : <p>8954678398</p>
+					insta id : <p>arul_sheebha</p>
+					youtube : <p>tigerthejermanshepherd</p>
+
+		
+		</body>
+</html>
